54cd5ad3cce17de4007dc023eb4508cc2bb6240e
aria313202 <aria.313@gmail.com>
2023-11-21 21:52:22 +0330

    Revert "add vue frontend"
    This reverts commit 98b4ac65fcb2143dda5232dbc75a9c1331ce3380.

--- laravel-breez-api/app/Providers/AuthServiceProvider.php
+++ laravel-breez-api/app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,12 +25,6 @@ class AuthServiceProvider extends ServiceProvider
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
-        });
-
-        // Implicitly grant "Super Admin" role all permissions
-        // This works in the app by using gate-related functions like auth()->user->can() and @can()
-        Gate::before(function ($user, $ability) {
-            return $user->hasRole('Super Admin') ? true : null;
         });
 
         //
