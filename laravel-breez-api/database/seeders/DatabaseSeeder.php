c1440249bd574147bc0d93982168279ed8069d00
aria313202 <aria.313@gmail.com>
2023-11-21 19:02:15 +0330

    add seeder users,roles,permision

--- laravel-breez-api/database/seeders/DatabaseSeeder.php
+++ laravel-breez-api/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +23,48 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit users']);
+        Permission::create(['name' => 'delete users']);
+        Permission::create(['name' => 'active users']);
+        Permission::create(['name' => 'deactive users']);
+
+
+
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleAdmin->givePermissionTo([
+            'edit users',
+            'active users'
+        ]);
+
+
+        $roleSuperAdmin = Role::create(['name' => 'Super-Admin']);
+        // gets all permissions via Gate::before rule; see AuthServiceProvider
+
+        // create demo users
+        $user = \App\Models\User::factory()->create([
+            'name' => 'CustomerUser',
+            'email' => 'test@example.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'ManagerUser',
+            'email' => 'manager@example.com',
+            'password' => Hash::make('12345678')
+        ]);
+        $user->assignRole($roleAdmin);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Super-Admin',
+            'email' => 'superadmin@example.com',
+            'password' => Hash::make('12345678')
+        ]);
+        $user->assignRole($roleSuperAdmin);
     }
 }
