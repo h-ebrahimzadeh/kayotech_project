67a1b8694935d4fa63608f82c219d8e1d28fa174
aria313202 <aria.313@gmail.com>
2023-11-21 21:47:10 +0330

    add vue authentication frontend

--- laravel-breez-api/routes/api.php
+++ laravel-breez-api/routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['role:admin']], function () {
+    Route::group([
+        'prefix' => 'admin',
+        'as' => 'admin.',
+    ], function () {
+        Route::put('/users/update/{user}',[UserController::class,'update']);
+
+    });
+
+});
